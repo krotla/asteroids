d6586f0793f102b5a7b8f471a4df1c29ad08510b
krotla <kkornel@o2.pl>
2025-02-17 23:43:04 +0100

    feat: Add player sprite object. Add spaceship basic movement.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -15,12 +16,24 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return     
+        
+        updatable.update(dt)
 
-        screen.fill(color="black")
+        screen.fill(color="black")        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
