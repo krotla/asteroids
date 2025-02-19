d10ef495cb98633705299f32d5a5332dbb2fe53b
krotla <kkornel@o2.pl>
2025-02-19 23:28:21 +0100

    feat: Player can shoot (create bullets)

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
 
@@ -18,13 +19,6 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
-    def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
-
-    def move(self, dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
-    
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
@@ -39,3 +33,18 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shot(dt)
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def shot(self, dt):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+    
+    
